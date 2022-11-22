23d342220a93421163f61033edf1ee8771d55dec
ChaidenD <chaidendomingo@gmail.com>
2022-11-21 21:25:38 -0800

    feat: created spring boot project outline in java

---
+++ backend/src/myApplication/MainApplication.java
@@ -0,0 +1,12 @@
+package MyApplication;
+import org. springframework.boot.springApplication; 
+import org.springframework.boot.autoconfigure.SprinpootApplication; 
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
